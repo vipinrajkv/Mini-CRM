8465b9e34219eec7b71a06db04c4b8494f001dd4
vipinrajkv <vipinrajsree@gmail.com>
2024-11-18 10:35:41 +0530

    auth route changes

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,17 @@ use App\Http\Controllers\ProjectController;
 //     return view('welcome');
 // });
 
-Route::get('/', [App\Http\Controllers\UserController::class, 'index'])->name('index');
-Route::get('/task', [App\Http\Controllers\TaskController::class, 'index'])->name('index.tasks');
+
 
 Route::middleware(['auth', 'role:admin'])->group(function (){
+Route::get('/', [App\Http\Controllers\TaskController::class, 'index'])->name('index.tasks');
+Route::get('/task', [App\Http\Controllers\TaskController::class, 'index'])->name('index.tasks');
 Route::resource('projects', App\Http\Controllers\ProjectController::class);
 Route::resource('users', App\Http\Controllers\UserController::class);
 Route::resource('clients', App\Http\Controllers\ClientController::class);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 });
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/admin', [App\Http\Controllers\UserController::class, 'index'])->name('user.list');
